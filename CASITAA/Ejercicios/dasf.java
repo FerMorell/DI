79cab0fc34c0943950daa420e9d20c2a5be5c1f3
Fer <158490892+FerMorell@users.noreply.github.com>
2024-11-13 20:26:19 +0100

    Add files via upload

---
+++ CASITAA/Ejercicios/dasf.java
@@ -0,0 +1,9 @@
+/**
+ * dasf
+ */
+public class dasf {
+
+    public static void main(String[] args) {
+        ProcessBuilder pb= new ProcessBuilder("https://www.google.com/webhp?hl=es&sa=X&ved=0ahUKEwi018WT1c-IAxX4gP0HHbVLBCQQPAgI", "GOOGLE");
+    }
+}
